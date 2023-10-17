4910f2a1744f9cc6b066eca202a8e220d6a6a7d5
inikonzs <zhanzs@mail.ru>
2023-10-17 21:26:16 +0200

    add sprites

--- client/src/three/cellView.tsx
+++ client/src/three/cellView.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { IVector } from './game';
-import cell1 from './imgs/cell1.svg';
-import cell2 from './imgs/cell2.svg';
-import cell3 from './imgs/cell3.svg';
-import cell4 from './imgs/cell4.svg';
+import cell1 from './imgs/tomato.png';//'./imgs/cell1.svg';
+import cell2 from './imgs/cucu.png';
+import cell3 from './imgs/carrot.png';
+import cell4 from './imgs/berries.png';
+import boxh0 from './imgs/box_0h.png'
+import boxh1 from './imgs/box_1h.png';
+import boxh2 from './imgs/box_2h.png';
+import boxh3 from './imgs/box_3h.png';
+import bomb from './imgs/bomb.png';
+import rocket from './imgs/rocket.png';
+import heli from './imgs/heli.png';
+import disco from './imgs/disco.png';
 import { GameObject } from "./items/gameObject";
 
 export function CellView({setDragStart, cell, isTo, isFrom}: {setDragStart: (e: { cell: IVector, position: IVector })=>void, cell: GameObject, isTo?: boolean, isFrom?: boolean}){
@@ -33,7 +41,7 @@ export function CellView({setDragStart, cell, isTo, isFrom}: {setDragStart: (e: 
     }
   
     const cellClassName = `cell2 ${isFrom ? 'cell2_from' : ''} ${isTo ? 'cell2_to' : ''}`;
-    const backgroundColor = ['#fff', '#f000', '#00f0', '#0f00', '#ff00', '#f0f', '#999', '#f90', '#444', '#0ff', '#4949', '#9f99', '#2999', '#606'][Number(cell)];
+    const backgroundColor = "#0000"//['#fff', '#f000', '#00f0', '#0f00', '#ff00', '#f0f', '#999', '#f90', '#444', '#0ff', '#4949', '#9f99', '#2999', '#606'][Number(cell)];
   
     return <div 
       className={cellClassName} 
@@ -71,6 +79,6 @@ export function CellView({setDragStart, cell, isTo, isFrom}: {setDragStart: (e: 
         })()
       }
       </div>}
-      {<div className="cell-img" style={{'backgroundImage': 'url('+[null, cell1, cell2, cell3, cell4][Number(cell)]+')'}}></div>}
+      {<div className="cell-img" style={{'backgroundImage': 'url('+[null, cell1, cell2, cell3, cell4, [boxh0, boxh1, boxh2, boxh3][cell.health], null, rocket, bomb, disco, null, null, heli][Number(cell)]+')'}}></div>}
     </div>
   }

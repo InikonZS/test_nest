35712364e51031096cf3901740d8cba6682d57d9
inikonzs <zhanzs@mail.ru>
2024-02-28 03:15:30 +0100

    sparkle test

--- client/src/ui/app.tsx
+++ client/src/ui/app.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { AvatarEditor } from "./components/avatarEditor/avatarEditor";
+import { Sparkle } from "./components/sparkle/sparkle";
 
 export function App(){
     return <div>
         <AvatarEditor></AvatarEditor>
+        <Sparkle></Sparkle>
     </div>
 }
--- client/src/ui/components/sparkle/sparkle.tsx
+++ client/src/ui/components/sparkle/sparkle.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import './sparkle.css';
 
 export function Sparkle(){
-    return <div>
-        
+    const rand = ()=> Math.floor(Math.random() * 100) - 50;
+    const particles = new Array(10).fill(null).map((_, i)=>{
+        return <div className="sparkle_particle" style={{'offset-path': `path("M0,0 C${0},${0} ${rand()},${rand()} ${rand()},${rand()}")`, 'animation-delay': i*30+'ms'}}></div>
+    });
+    return <div className="sparkle_demo_wrapper">
+        <div className="sparkle_container">
+            {particles}
+        </div>
     </div>
 }

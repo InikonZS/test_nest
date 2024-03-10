bd9fb28da43f4c3c6b4cb4c8a7122b60bbe86c92
inikonzs <zhanzs@mail.ru>
2024-03-10 17:53:54 +0100

    wf update egg sprites

--- client/src/wf/components/collectable/collectable.tsx
+++ client/src/wf/components/collectable/collectable.tsx
@@ -8,17 +8,11 @@ interface ICollectableProps{
     onCollect: ()=>void;
 }
 
-const colors = {
-    'egg0': '#090',
-    'egg1': '#f90',
-    'egg2': '#f9f'
-}
-
 export function Collectable({itemData, onCollect}: ICollectableProps){
     const {assets} =  useContext(AssetsContext);
     return <div className="wf_collectable"
     onClick={onCollect}
-    style={{left: itemData.position.x, top: itemData.position.y, 'background-color': colors[itemData.type as keyof typeof colors], 'background-image': `url(${assets[itemData.type].objectUrl})`}}
+    style={{left: itemData.position.x, top: itemData.position.y, 'background-image': `url(${assets[itemData.type].objectUrl})`}}
     >
     </div>
 }
